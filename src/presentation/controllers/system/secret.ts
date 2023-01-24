7e7c11bc823712dcc84832008eeff0ca1f7fdab8
Artur Donda <artur.donda@outlook.com>
2023-01-24 20:56:18 -0300

    Testing AWS Codebuild env vars #5

--- src/presentation/controllers/system/secret.ts
+++ src/presentation/controllers/system/secret.ts
@@ -1,22 +1,22 @@
 import { errorHandler, ok } from '@presentation/helpers';
 import { HttpRequest, HttpResponse, IController } from '@presentation/protocols';
-import { SSM } from '@aws-sdk/client-ssm';
+// import { SSM } from '@aws-sdk/client-ssm';
 
 export class SecretController implements IController {
 	async handle(request: HttpRequest): Promise<HttpResponse<any>> {
 		try {
-			const ssmClient = new SSM({ region: 'us-east-1' });
+			// const ssmClient = new SSM({ region: 'us-east-1' });
 
-			const parameters = await ssmClient
-				.getParametersByPath({ Path: '/CodeBuild/', WithDecryption: true })
-				.then(result => result.Parameters);
+			// const parameters = await ssmClient
+			// 	.getParametersByPath({ Path: '/CodeBuild/', WithDecryption: true })
+			// 	.then(result => result.Parameters);
 
 			return ok({
 				message: 'Secrets Manager',
 				result: {
 					processEnv: process.env,
 					processEnvSecrets: process.env.secrets,
-					getParametersByPath: parameters,
+					// getParametersByPath: parameters,
 				},
 			});
 		} catch (error) {
