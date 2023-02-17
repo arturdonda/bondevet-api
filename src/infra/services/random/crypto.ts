ba8e7fe4aadd1d58c53c1e14ef72e466b7348621
Artur Donda <artur.donda@outlook.com>
2023-02-16 21:49:59 -0300

    Bugfix: crypto random string

--- src/infra/services/random/crypto.ts
+++ src/infra/services/random/crypto.ts
@@ -3,6 +3,12 @@ import crypto from 'crypto';
 
 export class RandomService implements IRandomService {
 	string({ length }: IRandomService.String.Params): IRandomService.String.Result {
-		return crypto.randomBytes(length ?? 32).toString('hex');
+		if (length <= 0) throw new Error('Invalid length: must be greater than 0');
+
+		const isOdd = !!(length % 2);
+
+		const randomString = crypto.randomBytes(isOdd ? (length + 1) / 2 : length / 2).toString('hex');
+
+		return isOdd ? randomString.slice(0, -1) : randomString;
 	}
 }
