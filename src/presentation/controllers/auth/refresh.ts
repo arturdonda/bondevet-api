f93295cec6d1610d637fc1f8fae00c9943147c95
Artur Donda <artur.donda@outlook.com>
2023-02-17 00:24:12 -0300

    Refresh Session Controller returns auth header

--- src/presentation/controllers/auth/refresh.ts
+++ src/presentation/controllers/auth/refresh.ts
@@ -5,20 +5,24 @@ import { HttpRequest, HttpResponse, IController } from '@presentation/protocols'
 
 export class RefreshSessionController implements IController {
 	constructor(private readonly service: IRefreshSession) {}
-	async handle(request: HttpRequest): Promise<HttpResponse<{ accessToken: string }>> {
+	async handle(request: HttpRequest): Promise<HttpResponse<null>> {
 		try {
 			if (!request.body.browser) throw new MissingParamError('browser');
 			if (!request.body.deviceType) throw new MissingParamError('deviceType');
+			if (!request.cookies?.refreshToken) throw new Error('Missing Session Token');
 
-			const result = await this.service.exec({
-				userId: request.userId,
+			const { accessToken } = await this.service.exec({
 				ipAddress: request.ip,
 				browser: request.body.browser,
 				deviceType: request.body.deviceType,
 				refreshToken: request.cookies.refreshToken,
 			});
 
-			return ok({ message: 'Session refreshed successfully', result });
+			return ok({
+				message: 'Session refreshed successfully',
+				result: null,
+				headers: { authorization: accessToken },
+			});
 		} catch (error) {
 			return errorHandler(error, 'Error refreshing session');
 		}
