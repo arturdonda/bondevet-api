d438a9fc32a32e2be2fb70784dd300ffdf405bc2
Artur Donda <artur.donda@outlook.com>
2023-02-17 00:23:10 -0300

    Parse cookies

--- src/main/config/express.ts
+++ src/main/config/express.ts
@@ -3,6 +3,7 @@ import '../../../module-alias';
 import { configureDb, configureLogger, configureRoutes } from '@main/config';
 import { createApiResponse } from '@main/middlewares';
 import express, { Express } from 'express';
+import cookieParser from 'cookie-parser';
 
 export const configureApp = async (): Promise<Express> => {
 	const app = express();
@@ -12,6 +13,7 @@ export const configureApp = async (): Promise<Express> => {
 
 	// Setup middlewares
 	app.use(express.json());
+	app.use(cookieParser());
 
 	// Setup routes
 	configureRoutes(app);
